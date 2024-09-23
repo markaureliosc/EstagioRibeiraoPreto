74266a358ac44e2164887444dcae33b161e4a9eb
Marco Aurélio <49064281+markaureliosc@users.noreply.github.com>
2024-09-23 15:09:51 -0300

    Create questao1.java

---
+++ questao1.java
@@ -0,0 +1,25 @@
+public class Main {
+  public static void main(String args[]) {
+    int F [] = new int [300];
+    
+    int teste = 13;
+    int v = 0;
+    
+    F[0] = 0;
+    F[1] = 1;
+    
+    for (int i = 2; i < 100; i++) {
+        F[i] = F[i-2] + F[i-1];
+        
+        if (teste == F[i]) {
+            v = 1;
+        }
+    }
+    
+    if (v == 0) {
+        System.out.printf("O número não está na sequência!");
+    } else {
+        System.out.printf("O número está na sequência!");
+    }
+  }
+}
